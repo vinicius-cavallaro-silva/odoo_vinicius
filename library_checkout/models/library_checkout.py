e0447c643d61a0c6edcd2e32dc815703b69ab16d
vinicius.cavallaro <vinicius.cavallaro.silva@gmail.com>
2022-11-09 09:38:01 -0300

    chapter_11

--- library_checkout/models/library_checkout.py
+++ library_checkout/models/library_checkout.py
@@ -118,6 +118,20 @@ class Checkout(models.Model):
             checkout.stage_id = done_stage
         return True
 
+    color = fields.Integer('Color Index')
+    priority = fields.Selection(
+        [('0', 'Low'),
+         ('1', 'Normal'),
+         ('2', 'High')],
+        'Priority',
+        default='1')
+    kanban_state = fields.Selection(
+        [('normal', 'In Progress'),
+         ('blocked', 'Blocked'),
+         ('done', 'Ready for next stage')],
+        'Kanban State',
+        default='normal')
+
 
 class CheckoutLine(models.Model):
     _name = 'library.checkout.line'
