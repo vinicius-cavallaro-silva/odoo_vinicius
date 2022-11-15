d8b8af1eba0eb81b43dd42947e7a8ab7ce62c2f9
vinicius.cavallaro <vinicius.cavallaro.silva@gmail.com>
2022-11-15 11:32:58 -0300

    teste_module

---
+++ teste/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2022 PCCube
+# @author: mrT <vcavallarosilva@pccube.com>
+{
+    'name': 'Teste',
+    'version': '13.0.1.0.0',
+    'category': 'Pccube',
+    'description': """
+            Module made to make some tests
+            """,
+    'author': 'Tiago Magrini Rigo, PcCube',
+    'website': "https://www.pccube.com",
+    "depends": [],
+    "data": [
+        'security/ir.model.access.csv',
+        'views/teste_view.xml',
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'application': True,
+}
